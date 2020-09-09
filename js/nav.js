04f889c00843b33ce663fb0451b88ec41c866f2c
PedroBailaAndrade <52913878+PedroBailaAndrade@users.noreply.github.com>
2020-09-09 10:15:51 +0100

    Content Selector

--- js/nav.js
+++ js/nav.js
@@ -1,14 +1,21 @@
-function oi () {
-    console.log('oi')
-}
+function contentDisplay(a, b, c, d, e) {
+    var a = document.getElementsByClassName(a)[0];
+    var b = document.getElementsByClassName(b)[0];
+    var c = document.getElementsByClassName(c)[0];
+    var d = document.getElementsByClassName(d)[0];
+    var e = document.getElementsByClassName(e)[0];
 
-oi();
-
-function contentDisplay(e) {
-    var x = document.getElementsByClassName(e)
-    if (x.style.display === "none") {
-        x.style.display = "grid";
+    if (a.style.display === "grid") {
+        a.style.display = "none";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "block";
     } else {
-        x.style.display = "none";
+        a.style.display = "grid";
+        b.style.display = "none";
+        c.style.display = "none";
+        d.style.display = "none";
+        e.style.display = "none";
     }
 }
