997b3a127393c803647059e861ca6fcb16818bd6
PedroBailaAndrade <52913878+PedroBailaAndrade@users.noreply.github.com>
2020-09-09 16:10:54 +0100

    Content Display v.1
    Trying to select the content through the nav
    
    -not finished

--- js/nav.js
+++ js/nav.js
@@ -1,25 +1,55 @@
-function contentDisplay(a, b, c, d, e, f) {
-    var a = document.getElementsByClassName(a)[0];
-    var b = document.getElementsByClassName(b)[0];
-    var c = document.getElementsByClassName(c)[0];
-    var d = document.getElementsByClassName(d)[0];
-    var e = document.getElementsByClassName(e)[0];
-    var f = document.getElementById(f).children[0];
+function contentDisplay(z) {
+    
+    var z = document.getElementsByClassName(z)[0];
 
-    if (a.style.display === 'grid') {
+    var a = document.getElementsByClassName('content-text')[0];
+
+    var b = document.getElementsByClassName('checkbox-content')[0];
+    var c = document.getElementsByClassName('radio-content')[0];
+    var d = document.getElementsByClassName('inputs-content')[0];
+    var e = document.getElementsByClassName('buttons-content')[0];
+    
+    var f = document.getElementById('checkbox').children[0];
+    var g = document.getElementById('radio').children[0];
+    var h = document.getElementById('inputs').children[0];
+    var i = document.getElementById('buttons').children[0];
+
+    if (z == b || z == c || z == d || z == e) {
         a.style.display = 'none';
-        b.style.display = 'none';
-        c.style.display = 'none';
-        d.style.display = 'none';
-        e.style.display = 'block';
-        a.classList.remove('active');  
     } else {
-        a.style.display = 'grid';
+        a.style.display = 'block';
+    }
+
+    if (z == b) {
+        b.style.display = 'grid';
+        f.style.color = '#f8ab37';
+    } else if (z != b) {
         b.style.display = 'none';
+        f.style.color = '#000000'
+    }
+
+    if (z == c) {
+        c.style.display = 'grid';
+        g.style.color = '#f8ab37';
+    } else if (z != c) {
         c.style.display = 'none';
+        g.style.color = '#000000'
+    }
+
+    if (z == d) {
+        d.style.display = 'grid';
+        h.style.color = '#f8ab37';
+    } else if (z != d) {
         d.style.display = 'none';
+        h.style.color = '#000000'
+    }
+
+    if (z == e) {
+        e.style.display = 'grid';
+        i.style.color = '#f8ab37';
+    } else if (z != e) {
         e.style.display = 'none';
-        a.classList.add('active');     
-    } 
+        i.style.color = '#000000'
+    }
 
 }
